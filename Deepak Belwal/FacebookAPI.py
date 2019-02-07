b71243d942108c3609e48f8752d6466ad91af541
deepbelwal <deepu123belwal321@gmail.com>
2019-02-07 22:29:12 +0530

    Hey i have done

--- Deepak Belwal/FacebookAPI.py
+++ Deepak Belwal/FacebookAPI.py
@@ -6,34 +6,48 @@ import urllib3
 from pandas import DataFrame
 from urllib.request import urlretrieve
 
-token = 'EAAEJotUciGgBAGfZB8koZCyeFY76jtuwb4Ib9gK5P90FiwnExUxKqH3a9EkohVpR2n9tS5Wiyi3ZBEKZBcpVr4NM6BsI7ZCDhSZCE1aETCEJAVFPVMTZBjMiqjl2TniZCaVU1yOclg8EkMscceXGqgm1ZB2dBAy7LkQNstj9CCW8ZAPRZCSlZA55iLrFWhddE6SPCUjzokjVzt0jgAZDZD'
+token = 'EAAEJotUciGgBAL1XvV9uwGuKImmPcfRhMZAawy0CZCC4MGzUMqZCbwEZBZARLxKnV5R60I5Ffms57XIZBnbsmozp7arnu0sPby4SOAO4RgdErXrqcl34A04PBwW60zlu5mE5c5ZC0Fu1DAaTBn4tPbqZAGJDWpWGmYWdaZC0oNQDXC9fZA6POpzLVdo4AQJ9TaegTx5ImucMfrQwZDZD'
 class FbData:
     global token
 
-    def get_photos(self):
+    def get_info(self):                             # To get info of the user
+        me='https://graph.facebook.com/v3.2/me?fields=id,name&access_token=' + token
+        me1=requests.get(me)
+        print(me1.text)
+
+    def get_photos(self):                           # To get photos into the .csv file
         photos = 'https://graph.facebook.com/v3.2/me?fields=id,name,photos{picture,link}&access_token=' + token
         s1=requests.get(photos)
         s1=s1.json()
         df =DataFrame(s1['photos']['data'])
         export_csv=df.to_csv("/home/nineleaps/PycharmProjects/PythonProgs/ExtractData/Names.csv")
 
-    def download_photos(self):
+
+    def get_download_links(self):                     # Funtion to Download photos
+
         with open('Names.csv', 'r') as f:
             reader = csv.reader(f)
             your_list = list(reader)
-            your_list.pop(0)
+            your_list.pop(0)                        # To remove the first element of list as it is of no  use
             l=len(your_list)
 
-            for i,j in enumerate(your_list):
+            for i,j in enumerate(your_list):        # traversing the list to find the links of photos
                 print(j[-1])
                 self.download(j[-1], i)
 
+    def download(self,url,dest_path):               # function to download the photos by traversing the each links
+                                                    # present in the list
+        urlretrieve(url, "Image{}".format(dest_path)+".jpg")        #retrieving the photo from the link availabe and
+                                                                    #storing them into the destination path with files
+                                                                    #names as Image{1,2,3,4,5,6,7,8........}
 
-    def download(self, url, dest_path):
-        urlretrieve(url, "Image{}".format(dest_path)+".jpg")
+f1  =  FbData()                                     #creating object of the FbData Class
+f1.get_info()
 
-f1 = FbData()
-f1.download_photos()
 
+
+
+f1.get_photos()
+f1.get_download_links()                              #calling the GetdownloadLink_methods to get the link.
 
 
